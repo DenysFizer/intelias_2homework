be5072a6b692fdeefc6ef6ac47d56432d9a7da53
Денис Фізер <Denys.Fizer.KNM.2019@edu.lpnu.ua>
2022-06-22 13:10:54 +0300

    second commit

---
+++ task2/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := "1 9 3 4 -5"
+	var result string
+	_ = result
+	var min, max int
+	//var num int
+	numsinString := strings.Fields(input)
+	var numsArr []int
+
+	for _, i := range numsinString {
+		j, err := strconv.Atoi(i)
+		if err != nil {
+			panic(err)
+		}
+		numsArr = append(numsArr, j)
+	}
+	max = numsArr[0]
+	min = numsArr[0]
+
+	for i := 0; i < len(numsArr); i++ {
+		switch {
+		case numsArr[i] > max:
+			max = numsArr[i]
+		case numsArr[i] < min:
+			min = numsArr[i]
+		}
+	}
+	if max == min {
+		result = result + strconv.Itoa(max)
+	} else {
+		result = result + strconv.Itoa(min) + " " + strconv.Itoa(max)
+	}
+	fmt.Println(result)
+
+}
